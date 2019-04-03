da5407ac964146d3137b240348e647dba2766859
Tom Malkin <tommalkin28@gmail.com>
2019-04-03 22:39:32 +1100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,48 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('simqle/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+REQUIRES = ['sqlalchemy', 'pyyaml']
+
+setup(
+    name='simqle',
+    version=version,
+    description='The simple way to sql in python',
+    long_description=readme,
+    author='Tom Malkin',
+    author_email='tommalkin28@gmail.com',
+    maintainer='Tom Malkin',
+    maintainer_email='tommalkin28@gmail.com',
+    url='https://github.com/Harlekuin/SimQLe',
+    license='MIT',
+
+    keywords=[
+        'sql',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['codecov', 'pytest'],
+
+    packages=find_packages(),
+)
