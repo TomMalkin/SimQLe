474a858b04435187c8a13dd51ebe0c2166de8cbb
Tom Malkin <tommalkin28@gmail.com>
2019-06-08 18:11:16 +1000

    pass through params to internal

--- simqle/internal.py
+++ simqle/internal.py
@@ -9,24 +9,24 @@ def load_connections(connections_file="./.connections.yaml"):
     """Load the Internal connection manager."""
     global INTERNAL_CONNECTION_MANAGER
 
-    if INTERNAL_CONNECTION_MANAGER:
-        raise Exception("The internal connection manager has already been "
-                        "loaded, this would overwrite the current one.")
-        # TODO: add load details to the ConnectionManager class so we can tell
-        # the user at this point what module loaded the internal simqle
-        # ConnectionManager instance.
+    # if INTERNAL_CONNECTION_MANAGER:
+    #     raise Exception("The internal connection manager has already been "
+    #                     "loaded, this would overwrite the current one.")
+    # TODO: add load details to the ConnectionManager class so we can tell
+    # the user at this point what module loaded the internal simqle
+    # ConnectionManager instance.
 
     INTERNAL_CONNECTION_MANAGER = ConnectionManager(file_name=connections_file)
 
 
 def execute_sql(con_name, sql, params=None):
     """Execute sql on the Internal ConnectionManager."""
-    INTERNAL_CONNECTION_MANAGER.execute_sql(con_name, sql, params=None)
+    INTERNAL_CONNECTION_MANAGER.execute_sql(con_name, sql, params=params)
 
 
 def recordset(con_name, sql, params=None):
     """Return SQL results from the Internal ConnectionManager."""
-    INTERNAL_CONNECTION_MANAGER.recordset(con_name, sql, params=None)
+    INTERNAL_CONNECTION_MANAGER.recordset(con_name, sql, params=params)
 
 
 def get_connection(con_name):
