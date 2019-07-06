7a5f40732601756c0edaecb01a73f4224cc57306
Tom Malkin <tommalkin28@gmail.com>
2019-07-07 00:56:47 +1000

    update readme

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     ],
 
     install_requires=REQUIRES,
-    tests_require=["codecov", "pytest", "coverage"],
+    tests_require=["codecov", "pytest", "coverage", "behave"],
 
     packages=find_packages(),
 )
