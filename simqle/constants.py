3c5f821bf71241bae67edb9b56167a02d56e070e
Tom Malkin <tommalkin28@gmail.com>
2019-11-16 17:45:05 +1100

    add home directory to list of default connections

--- simqle/constants.py
+++ simqle/constants.py
@@ -1,11 +1,15 @@
 """Load Constants."""
+from os.path import expanduser, join
+
 DEV_MAP = {
     "production": "connections",
     "development": "dev-connections",
     "testing": "test-connections",
 }
 
-# TODO: add other default locations.
 DEFAULT_FILE_LOCATIONS = [
     "./.connections.yaml",
+
+    # the home folder on either Linux or Windows
+    join(expanduser("~"), ".connections.yaml")
 ]
